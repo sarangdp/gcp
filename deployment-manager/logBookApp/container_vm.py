8d637fb57253f48b0a33a394290690a5f023792e
Sarang Panchawadkar <sarangdp@gmail.com>
2020-05-27 08:36:15 -0400

    updated the import

--- deployment-manager/logBookApp/container_vm.py
+++ deployment-manager/logBookApp/container_vm.py
@@ -1,3 +1,5 @@
+from .container_helper import GenerateManifest
+
 COMPUTE_URL_BASE = 'https://www.googleapis.com/compute/v1/'
 
 def ZonalComputeUrl(project, zone, collection, name):
@@ -24,7 +26,7 @@ def GenerateConfig(context):
         'metadata': {
             'items': [{
                 'key': 'gce-container-declaration',
-                'value': GenerateConfig(context)
+                'value': GenerateManifest(context)
             }]
         },
         'disk': [{
